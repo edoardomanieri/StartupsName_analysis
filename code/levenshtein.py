749bbcdfe965151d8ecd9f2e348212119af5c653
Luca Masserano <u6619605@anu.edu.au>
2018-11-16 13:50:14 +0100

    Levensthein completed

--- code/levenshtein.py
+++ code/levenshtein.py
@@ -28,14 +28,15 @@ import ctypes
 
 #    return res
 
+#cd '/Users/lucamasserano/Desktop/BOCCONI/Business Analytics/Final project/Business_Analytics/code'
 
 _lev = ctypes.CDLL('./liblev.so')
 _lev.levenshtein.argtypes = (ctypes.c_char_p, ctypes.c_char_p)
 _lev.levenshtein.restypes = ctypes.c_uint
 
-def find_all_substring(string, n ):
+def find_all_substrings(string, min_number_of_letters):
     res = []
-    for i in range(n, len(string)):
+    for i in range(min_number_of_letters, len(string)):
         res += substrings(string, i)
     return res
 
@@ -48,17 +49,19 @@ def substrings(string, number_of_letters):
         res.append(string[i: i + number_of_letters])
     return res
 
-def levenshtein_max(list_of_strings, substring, param):
+def levenshtein_max(company_names, substring, param):
     res = 0
+    common_companies = []
     substring = str(substring)
     c_substring = substring.encode('utf-8')
-    for comp in list_of_strings:
+    for comp in company_names:
         for elem in comp.split():
             elem = str(elem)
             c_elem = elem.encode('utf-8')
-            lev = _lev.levenshtein(c_elem, c_substring)
+            dist = _lev.levenshtein(c_elem, c_substring)
             if len(c_elem) > len(c_substring):
-                lev -= len(c_elem) - len(c_substring)
-            if lev <= param:
+                dist -= len(c_elem) - len(c_substring)
+            if dist <= param:
                 res += 1
-    return res
+                common_companies.append(comp)
+    return res, common_companies
